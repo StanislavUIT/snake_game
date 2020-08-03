dd76013e6295f860bc0c30ff4bdeaaf0de38e248
StanislavUIT <klibashev.s@beldor.centr.by>
2020-08-03 18:24:59 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+import pygame
+
+FRAME_COLOR = (0, 255, 204)
+WHITE = (255, 255, 255)
+BLUE = (204, 255, 255)
+HEDADER_COLOR = (0, 204, 153)
+SNAKE_COLOR = (0, 102, 0)
+SIZE_BLOCK = 20
+COUNT_BLOCKS = 20
+HEDADER_MARGIN = 70
+MARGIN = 1
+size = [SIZE_BLOCK * COUNT_BLOCKS + 2 * SIZE_BLOCK + MARGIN * COUNT_BLOCKS,
+        SIZE_BLOCK * COUNT_BLOCKS + 2 * SIZE_BLOCK + MARGIN * COUNT_BLOCKS + HEDADER_MARGIN]
+print(size)
+
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Змейка")
+
+
+class SnakeBlock:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+def draw_block(color, row, column):
+    pygame.draw.rect(screen, color, [SIZE_BLOCK + column * SIZE_BLOCK + MARGIN * (column + 1),
+                                     HEDADER_MARGIN + row * SIZE_BLOCK + MARGIN * (row + 1) + SIZE_BLOCK,
+                                     SIZE_BLOCK,
+                                     SIZE_BLOCK])
+
+
+snake_block = [SnakeBlock(9, 9)]
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    screen.fill(FRAME_COLOR)
+    pygame.draw.rect(screen, HEDADER_COLOR, [0, 0, size[0], HEDADER_MARGIN])
+
+    for row in range(COUNT_BLOCKS):
+        for column in range(COUNT_BLOCKS):
+            if (row + column) % 2 == 0:
+                color = BLUE
+            else:
+                color = WHITE
+            draw_block(color, row, column)
+    for block in snake_block:
+        draw_block(SNAKE_COLOR, block.x, block.y)
+    pygame.display.flip()
